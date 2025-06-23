c68bc558f80f9adb43560e1ec9373a5665684ec0
AlyaZahra <alyazahs13@gmail.com>
2025-06-23 21:11:32 +0700

    layout login

--- fe_sistem_rs/app/(auth)/login/page.tsx
+++ fe_sistem_rs/app/(auth)/login/page.tsx
@@ -1,46 +1,95 @@
-/* eslint-disable @next/next/no-img-element */
-'use client';
-import { Button } from 'primereact/button';
-import { InputText } from 'primereact/inputtext';
-import React from 'react';
-import '@/styles/gradient.css';
+"use client";
 
-const LoginPage = () => {
-    return (
-        <div className="min-h-screen flex justify-content-center align-items-center">
-            <div className="animated-gradient-bg w-full">
-                <div className="card w-10 h-full md:h-30rem">
-                    <div className="grid h-full">
-                        <div className="col-12 md:col-6 flex flex-col justify-center h-full px-4">
-                            <div>
-                                <img src="/layout/images/logo.png" style={{ maxWidth: '100%' }} className="h-4rem md:h-5rem" alt="logo" />
-                                <h3 className="text-2xl font-bold mt-2 mb-10">Rumah Sakit</h3>
+import { useState } from "react";
+import axios from "axios";
+import Cookies from "js-cookie";
+import { useRouter } from "next/navigation";
+import { Button } from "primereact/button";
+import { InputText } from "primereact/inputtext";
+import React from "react";
+import "@/styles/gradient.css";
 
-                                <form className="grid">
-                                    <div className="col-12">
-                                        <label htmlFor="email">Email</label>
-                                        <InputText type="email" className="w-full mt-3" />
-                                    </div>
-                                    <div className="col-12">
-                                        <label htmlFor="password">Password</label>
-                                        <InputText type="password" className="w-full mt-3" />
-                                    </div>
+export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
 
-                                    <div className="col-12 mt-3">
-                                        <Button label="login" className="w-full" />
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
 
-                        <div className="hidden md:block md:col-6 h-full">
-                            <img src="/layout/images/login.png" className="w-full h-full object-cover" alt="cover" />
-                        </div>
-                    </div>
-                </div>
+    try {
+      const res = await axios.post("http://localhost:4000/login", {
+        email,
+        password,
+      });
+
+      Cookies.set("token", res.data.token);
+      router.push("/");
+      alert("Login berhasil!");
+    } catch (err) {
+      alert("Login gagal. email atau password salah.");
+      console.error("Login error:", err);
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex justify-content-center align-items-center">
+      <div className="animated-gradient-bg w-full">
+        <div className="card w-11/12 max-w-4xl md:h-30rem h-full">
+          <div className="grid h-full">
+            <div className="col-12 md:col-6 flex flex-col justify-center h-full px-4">
+              <div>
+                <img
+                  src="/layout/images/logo.png"
+                  style={{ maxWidth: "100%" }}
+                  className="h-4rem md:h-5rem"
+                  alt="logo"
+                />
+                <h3 className="text-2xl text-left font-semibold font-sans mt-2 mb-10">
+                  RUMAH SAKIT
+                </h3>
+
+                <form onSubmit={handleLogin} className="grid">
+                  <div className="col-12">
+                    <label htmlFor="email">Email</label>
+                    <InputText
+                      id="email"
+                      type="text"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      required
+                      className="w-full mt-3"
+                    />
+                  </div>
+                  <div className="col-12">
+                    <label htmlFor="password">Password</label>
+                    <InputText
+                      id="password"
+                      type="password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      required
+                      className="w-full mt-3"
+                    />
+                  </div>
+
+                  <div className="col-12 mt-3">
+                    <Button label="login" className="w-full" />
+                  </div>
+                </form>
+              </div>
             </div>
-        </div>
-    );
-};
 
-export default LoginPage;
+            <div className="hidden md:block md:col-6 h-full">
+              <img
+                src="/layout/images/login.png"
+                className="w-full h-full object-cover"
+                alt="cover"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
