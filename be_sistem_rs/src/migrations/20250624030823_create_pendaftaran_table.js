041b19181c66b88491afb668ad27b9b0a8e8bab5
Barleyan <iyan.manisrejo@gmail.com>
2025-06-25 13:36:18 +0700

    update migrasi pendaftaran

--- be_sistem_rs/src/migrations/20250624030823_create_pendaftaran_table.js
+++ be_sistem_rs/src/migrations/20250624030823_create_pendaftaran_table.js
@@ -3,19 +3,14 @@
  * @returns { Promise<void> }
  */
 export const up = function (knex) {
-  return knex.schema.createTable('pasien', (table) => {
-    table.increments('IDPASIEN').primary();
-    table.string('NIK', 20).unique().notNullable();
-    table.string('NAMALENGKAP', 100).notNullable();
-    table.date('TANGGALLAHIR').notNullable();
-    table.enu('JENISKELAMIN', ['L', 'P']).notNullable();
-    table.text('ALAMAT');
-    table.string('NOHP', 15);
-    table.string('AGAMA', 15);
-    table.string('GOLDARAH', 3);
-    table.enu('ASURANSI', ['BPJS', 'Umum', 'Lainnya']);
-    table.string('NOASURANSI', 30);
-    table.timestamp('TANGGALDAFTAR').defaultTo(knex.fn.now());
+  return knex.schema.createTable('pendaftaran', (table) => {
+    table.increments('IDPENDAFTARAN').primary();
+    table.integer('IDPASIEN').unsigned().notNullable();
+    table.date('TANGGALDAFTAR').defaultTo(knex.fn.now());
+    table.string('KETERANGAN', 255);
+
+    // Relasi ke tabel pasien
+    table.foreign('IDPASIEN').references('IDPASIEN').inTable('pasien').onDelete('CASCADE');
   });
 };
 
@@ -23,7 +18,6 @@ export const up = function (knex) {
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-
 export const down = function (knex) {
-  return knex.schema.dropTable('pasien');
+  return knex.schema.dropTable('pendaftaran');
 };
