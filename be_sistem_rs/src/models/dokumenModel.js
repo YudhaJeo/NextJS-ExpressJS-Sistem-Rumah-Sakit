dd5382b3089ca003a14660646d69837170c81ab4
Shalfa Amelia <shalfaamelia@gmail.com>
2025-06-25 19:53:58 +0700

    replace dokumen dan pasien model

--- be_sistem_rs/src/models/dokumenModel.js
+++ be_sistem_rs/src/models/dokumenModel.js
@@ -1,32 +1,27 @@
 import db from '../core/config/knex.js';
 
-const DokumenModel = {
-  getAll: () => {
-    return db('dokumen')
-      .join('pasien', 'dokumen.NIK', 'pasien.NIK') 
-      .select('dokumen.*', 'pasien.NAMALENGKAP');
-  },
-
-  getByNIK: (nik) => {
-    return db('dokumen').where({ NIK: nik });
-  },
-
-  getById: (id) => {
-    return db('dokumen').where({ IDDOKUMEN: id }).first();
-  },
-
-  create: (data) => {
-    return db('dokumen').insert(data);
-  },
-
-  update: (id, data) => {
-    return db('dokumen').where({ IDDOKUMEN: id }).update(data);
-  },
-
-  remove: (id) => {
-    return db('dokumen').where({ IDDOKUMEN: id }).del();
-  },
+export const getAll = () => {
+  return db('dokumen')
+    .join('pasien', 'dokumen.NIK', 'pasien.NIK')
+    .select('dokumen.*', 'pasien.NAMALENGKAP');
 };
 
+export const getById = (id) => {
+  return db('dokumen').where({ IDDOKUMEN: id }).first();
+};
 
-export default DokumenModel;
+export const getByNIK = (nik) => {
+  return db('dokumen').where({ NIK: nik });
+};
+
+export const create = (data) => {
+  return db('dokumen').insert(data);
+};
+
+export const update = (id, data) => {
+  return db('dokumen').where({ IDDOKUMEN: id }).update(data);
+};
+
+export const remove = (id) => {
+  return db('dokumen').where({ IDDOKUMEN: id }).del();
+};
--- be_sistem_rs/src/models/pasienModel.js
+++ be_sistem_rs/src/models/pasienModel.js
@@ -1,7 +1,21 @@
 import db from '../core/config/knex.js';
 
-export const getAll = () => db('pasien').select();
-export const getById = (id) => db('pasien').where({ IDPASIEN: id }).first();
-export const create = (data) => db('pasien').insert(data);
-export const update = (id, data) => db('pasien').where({ IDPASIEN: id }).update(data);
-export const remove = (id) => db('pasien').where({ IDPASIEN: id }).del();
+export const getAll = () => {
+  return db('pasien').select();
+};
+
+export const getById = (id) => {
+  return db('pasien').where({ IDPASIEN: id }).first();
+};
+
+export const create = (data) => {
+  return db('pasien').insert(data);
+};
+
+export const update = (id, data) => {
+  return db('pasien').where({ IDPASIEN: id }).update(data);
+};
+
+export const remove = (id) => {
+  return db('pasien').where({ IDPASIEN: id }).del();
+};
