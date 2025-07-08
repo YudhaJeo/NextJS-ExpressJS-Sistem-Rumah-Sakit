8d5068aa390f6a55c3d0c801a0ee8394964a450f
BagasYudha <bayud.kematian@gmail.com>
2025-07-08 10:12:36 +0700

    ADD: Halaman tampilan master jenis kamar dan GET

---
+++ sistem_rs_fe/app/(dashboard)/master/jeniskamar/components/tabelJenis.js
@@ -0,0 +1,45 @@
+// app/(dashboard)/master/asuransi/components/tabelAgama.js
+'use client';
+
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import { Button } from 'primereact/button';
+import React from 'react';
+
+const TabelJenis = ({ data, loading, onEdit, onDelete  }) => {
+  return (
+    <DataTable 
+      value={data} 
+      paginator rows={5} 
+      loading={loading} 
+      size="small">
+      <Column field="IDJENISKAMAR" header="ID" />
+      <Column field="NAMAJENIS" header="Jenis Kamar" />
+      <Column field="HARGA_PER_HARI" header="Harga" />
+      <Column field="FASILITAS" header="Fasilitas" />
+      
+      <Column
+        header="Aksi"
+        body={(row) => (
+          <div className="flex gap-2">
+            <Button
+              icon="pi pi-pencil"
+              size="small"
+              severity="warning"
+              onClick={() => onEdit(row)}
+            />
+            <Button
+              icon="pi pi-trash"
+              size="small"
+              severity="danger"
+              onClick={() => onDelete(row)}
+            />
+          </div>
+        )}
+        style={{ width: '150px' }}
+      />
+    </DataTable>
+  );
+};
+
+export default TabelJenis;
