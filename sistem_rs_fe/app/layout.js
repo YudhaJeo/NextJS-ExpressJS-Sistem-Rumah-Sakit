cf6f9a1f5ff52d016f71219e8da8bb3fb3f1a1f1
BagasYudha <bayud.kematian@gmail.com>
2025-07-08 11:16:47 +0700

    FIX: Perbaikan issue root layout merah kosong pada body

--- sistem_rs_fe/app/layout.js
+++ sistem_rs_fe/app/layout.js
@@ -1,23 +1,29 @@
-'use client';
-import { LayoutProvider } from '../layout/context/layoutcontext';
-import { PrimeReactProvider } from 'primereact/api';
+// app/layout.js
+import Providers from './providers';
 import 'primereact/resources/primereact.css';
 import 'primeflex/primeflex.css';
 import 'primeicons/primeicons.css';
 import '../styles/layout/layout.scss';
 import '../styles/demo/Demos.scss';
 
+export const metadata = {
+  title: 'Rumah Sakit',
+  description: 'Sistem Informasi Rumah Sakit',
+};
+
 export default function RootLayout({ children }) {
-    return (
-        <html lang="en" suppressHydrationWarning>
-            <head>
-                <link id="theme-css" href={`/themes/lara-light-indigo/theme.css`} rel="stylesheet"></link>
-            </head>
-            <body>
-                <PrimeReactProvider>
-                    <LayoutProvider>{children}</LayoutProvider>
-                </PrimeReactProvider>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="id" suppressHydrationWarning>
+      <head>
+        <link
+          id="theme-css"
+          href="/themes/lara-light-indigo/theme.css"
+          rel="stylesheet"
+        />
+      </head>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
 }
