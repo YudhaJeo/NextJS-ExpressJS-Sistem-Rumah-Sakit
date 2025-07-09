ba893464b50e8b14a7ecc2778f7815af99aef2e0
BagasYudha <bayud.kematian@gmail.com>
2025-07-09 14:59:17 +0700

    ADD: Migrasi untuk kamar dan bed

---
+++ sistem_rs_be/src/migrations/20250705111000_create_kamar_table.js.js
@@ -0,0 +1,15 @@
+// src/migrations/20250705111000_create_kamar_table.js
+export const up = function (knex) {
+    return knex.schema.createTable('kamar', (table) => {
+      table.increments('IDKAMAR').primary();
+      table.string('NAMAKAMAR', 100).notNullable().unique();
+      table.integer('IDBANGSAL').unsigned().notNullable()
+        .references('IDBANGSAL').inTable('bangsal').onDelete('CASCADE');
+      table.integer('KAPASITAS').unsigned().notNullable();
+      table.text('KETERANGAN');
+    });
+  };
+  
+  export const down = function (knex) {
+    return knex.schema.dropTable('kamar');
+  };
