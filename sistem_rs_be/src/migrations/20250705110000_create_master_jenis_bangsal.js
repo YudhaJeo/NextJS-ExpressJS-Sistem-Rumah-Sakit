a59fe96927d385b11588aed626a781e2d853d79e
BagasYudha <bayud.kematian@gmail.com>
2025-07-09 14:31:31 +0700

    UPD: Pecah manajemen kamar - Migrasi

--- sistem_rs_be/src/migrations/20250705110000_create_master_jenis_bangsal.js
+++ sistem_rs_be/src/migrations/20250705110000_create_master_jenis_bangsal.js
@@ -1,9 +1,9 @@
-// src/migrations/20250704131000_create_master_jenis_bangsal.js
+// src/migrations/20250705110000_create_master_jenis_bangsal.js
 export const up = function (knex) {
   return knex.schema.createTable('jenis_bangsal', (table) => {
     table.increments('IDJENISBANGSAL').primary();
     table.string('NAMAJENIS', 50).notNullable().unique();
-    table.specificType('HARGA_PER_HARI', 'DOUBLE').notNullable(); 
+    table.specificType('HARGA_PER_HARI', 'DOUBLE').notNullable();
     table.text('FASILITAS');
   });
 };
--- sistem_rs_be/src/migrations/20250705110500_create_bangsal_table.js
+++ sistem_rs_be/src/migrations/20250705110500_create_bangsal_table.js
@@ -1,15 +1,11 @@
-// src/migrations/20250704131500_create_bangsal_table.js
+// src/migrations/20250705110500_create_bangsal_table.js
 export const up = function (knex) {
   return knex.schema.createTable('bangsal', (table) => {
     table.increments('IDBANGSAL').primary();
     table.string('NAMABANGSAL', 100).notNullable().unique();
     table.integer('IDJENISBANGSAL').unsigned().notNullable()
-      .references('IDJENISBANGSAL')
-      .inTable('jenis_bangsal')
-      .onDelete('CASCADE');
-    table.integer('KAPASITAS').unsigned().notNullable(); 
-    table.integer('TERISI').unsigned().defaultTo(0); 
-    table.enu('STATUS', ['TERSEDIA', 'PENUH', 'DIBERSIHKAN']).notNullable().defaultTo('TERSEDIA');
+      .references('IDJENISBANGSAL').inTable('jenis_bangsal').onDelete('CASCADE');
+    table.string('LOKASI', 100);
     table.text('KETERANGAN');
   });
 };
