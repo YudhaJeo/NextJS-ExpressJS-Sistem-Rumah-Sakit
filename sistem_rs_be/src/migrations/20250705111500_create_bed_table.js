d068d7c18016e44fa6982e969c8cb4f2c3411ff3
BagasYudha <bayud.kematian@gmail.com>
2025-07-10 15:18:04 +0700

    UPD: Migration bed dengan created_at dan updated_at

--- sistem_rs_be/src/migrations/20250705111500_create_bed_table.js
+++ sistem_rs_be/src/migrations/20250705111500_create_bed_table.js
@@ -8,6 +8,8 @@
       table.enu('STATUS', ['TERSEDIA', 'TERISI', 'DIBERSIHKAN'])
         .notNullable().defaultTo('TERSEDIA');
       table.text('KETERANGAN');
+      table.timestamp('CREATED_AT').defaultTo(knex.fn.now());
+      table.timestamp('UPDATED_AT').defaultTo(knex.fn.now());
     });
   };
   
