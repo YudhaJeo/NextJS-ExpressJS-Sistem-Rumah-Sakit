cabaadf0a2aeb968690b14fcb85929d2c0f3545c
AlyaZahra <alyazahs13@gmail.com>
2025-07-14 11:22:15 +0700

    UPD: fe be dashboard pasien

--- sistem_rs_be/src/models/dashboardPasienModel.js
+++ sistem_rs_be/src/models/dashboardPasienModel.js
@@ -1,24 +1,24 @@
 import db from '../core/config/knex.js';
 
 export const getTotalPasien = () =>
-  db('PASIEN').count('IDPASIEN as total').first();
+  db('pasien').count('IDPASIEN as total').first();
 
 export const getPasienHariIni = () => {
   const today = new Date().toISOString().split('T')[0];
-  return db('PASIEN')
-    .where('TANGGALDAFTAR', today)
-    .count('IDPASIEN as total')
+  return db('pendaftaran')
+    .where('TANGGALKUNJUNGAN', today)
+    .count('IDPENDAFTARAN as total')
     .first();
 };
 
 export const getJumlahPerJenisKelamin = () =>
-  db('PASIEN')
+  db('pasien')
     .select('JENISKELAMIN')
     .count('IDPASIEN as total')
     .groupBy('JENISKELAMIN');
 
 export const getStatistikBulanan = () =>
-  db('PASIEN')
+  db('pasien')
     .select(
       db.raw("DATE_FORMAT(TANGGALDAFTAR, '%Y-%m') as bulan"),
       db.raw('COUNT(*) as total')
