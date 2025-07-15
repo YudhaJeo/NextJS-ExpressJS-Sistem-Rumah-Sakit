ea51bcfbd2df5247ea95478bde705347ad2675c1
AlyaZahra <alyazahs13@gmail.com>
2025-07-15 14:03:01 +0700

    UPD: filter tgl

--- sistem_rs_fe/app/(dashboard)/(pasien)/riwayatKunjungan/page.js
+++ sistem_rs_fe/app/(dashboard)/(pasien)/riwayatKunjungan/page.js
@@ -3,6 +3,7 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import FilterTanggal from '@/app/components/filterTanggal';
+import HeaderBar from '@/app/components/headerbar';
 import TabelRiwayatKunjungan from './components/tabelRiwayatKunjungan';
 
 
@@ -10,6 +11,7 @@ const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 const RiwayatKunjunganPage = () => {
   const [data, setData] = useState([]);
+  const [originalData, setOriginalData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(null);  
@@ -25,12 +27,24 @@ const RiwayatKunjunganPage = () => {
     try {
       const res = await axios.get(`${API_URL}/riwayatkunjungan`);
       setData(res.data.data);
+      setOriginalData(res.data.data);
     } catch (err) {
       console.error('Gagal mengambil data:', err);
     } finally {
       setLoading(false);
     }
   };
+
+    const handleSearch = (keyword) => {
+    if (!keyword) return setData(originalData);
+    const filtered = originalData.filter(
+      (item) =>
+        item.NIK.toLowerCase().includes(keyword.toLowerCase()) ||
+        item.NAMALENGKAP.toLowerCase().includes(keyword.toLowerCase())
+    );
+    setData(filtered);
+  };
+
 
     const handleDateFilter = () => {
     if (!startDate && !endDate) return setData(originalData);
@@ -53,6 +67,7 @@ const RiwayatKunjunganPage = () => {
     <div className="card">
       <h3 className="text-xl font-semibold mb-4">Riwayat Kunjungan</h3>
 
+      <div className="flex flex-col md:flex-row justify-content-between md:items-center gap-4">
         <FilterTanggal
           startDate={startDate}
           endDate={endDate}
@@ -61,6 +76,16 @@ const RiwayatKunjunganPage = () => {
           handleDateFilter={handleDateFilter}
           resetFilter={resetFilter}
         />
+        <HeaderBar
+          title=""
+          placeholder="Cari nama atau NIK..."
+          onSearch={handleSearch}
+          onAddClick={() => {
+            resetForm();
+            setDialogVisible(true);
+          }}
+        />
+      </div>
 
         <TabelRiwayatKunjungan data={data} loading={loading} />
 
