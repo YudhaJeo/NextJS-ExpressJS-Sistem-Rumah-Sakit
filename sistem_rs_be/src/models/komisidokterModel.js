1c66c36facb4f4d8269ef84b1ba402a0ffaded10
Barleyan <iyan.manisrejo@gmail.com>
2025-07-30 15:35:41 +0700

    revisi komisi

--- sistem_rs_be/src/models/komisidokterModel.js
+++ sistem_rs_be/src/models/komisidokterModel.js
@@ -4,16 +4,16 @@ export const getAllKomisi = () =>
   db('komisi_dokter')
     .join('riwayat_pengobatan', 'komisi_dokter.IDPENGOBATAN', 'riwayat_pengobatan.IDPENGOBATAN')
     .join('pendaftaran', 'riwayat_pengobatan.IDPENDAFTARAN', 'pendaftaran.IDPENDAFTARAN')
-    .join('pasien', 'riwayat_pengobatan.NIK', 'pasien.NIK')
     .join('dokter', 'riwayat_pengobatan.IDDOKTER', 'dokter.IDDOKTER')
+    .join('pasien', 'pendaftaran.NIK', 'pasien.NIK')
     .join('master_tenaga_medis', 'dokter.IDTENAGAMEDIS', 'master_tenaga_medis.IDTENAGAMEDIS')
     .select(
       'komisi_dokter.*',
-      'riwayat_pengobatan.TANGGALKUNJUNGAN',
-      'riwayat_pengobatan.KELUHAN',
+      'pendaftaran.NIK',
+      'pendaftaran.TANGGALKUNJUNGAN',
+      'pendaftaran.KELUHAN',
       'riwayat_pengobatan.DIAGNOSA',
       'riwayat_pengobatan.OBAT',
-      'pasien.NIK',
       'pasien.NAMALENGKAP as NAMAPASIEN',
       'master_tenaga_medis.NAMALENGKAP as NAMADOKTER'
     );
@@ -22,16 +22,16 @@ export const getByIdKomisi = (id) =>
   db('komisi_dokter')
     .join('riwayat_pengobatan', 'komisi_dokter.IDPENGOBATAN', 'riwayat_pengobatan.IDPENGOBATAN')
     .join('pendaftaran', 'riwayat_pengobatan.IDPENDAFTARAN', 'pendaftaran.IDPENDAFTARAN')
-    .join('pasien', 'riwayat_pengobatan.NIK', 'pasien.NIK')
     .join('dokter', 'riwayat_pengobatan.IDDOKTER', 'dokter.IDDOKTER')
+    .join('pasien', 'pendaftaran.NIK', 'pasien.NIK')
     .join('master_tenaga_medis', 'dokter.IDTENAGAMEDIS', 'master_tenaga_medis.IDTENAGAMEDIS')
     .select(
       'komisi_dokter.*',
-      'riwayat_pengobatan.TANGGALKUNJUNGAN',
-      'riwayat_pengobatan.KELUHAN',
+      'pendaftaran.NIK',
+      'pendaftaran.TANGGALKUNJUNGAN',
+      'pendaftaran.KELUHAN',
       'riwayat_pengobatan.DIAGNOSA',
       'riwayat_pengobatan.OBAT',
-      'pasien.NIK',
       'pasien.NAMALENGKAP as NAMAPASIEN',
       'master_tenaga_medis.NAMALENGKAP as NAMADOKTER'
     )
