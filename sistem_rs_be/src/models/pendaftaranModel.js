9a13ee7025446f9ff98011b6d3c39ca1db686551
BagasYudha <bayud.kematian@gmail.com>
2025-07-31 15:01:22 +0700

    FIX: Warnig returning postsgre sql

--- sistem_rs_be/src/models/pendaftaranModel.js
+++ sistem_rs_be/src/models/pendaftaranModel.js
@@ -1,30 +1,33 @@
+// sistem_rs_be\src\models\pendaftaranModel.js
 import db from '../core/config/knex.js';
 
-export const getAll = () =>{
+export const getAll = () => {
   return db('pendaftaran')
     .join('pasien', 'pendaftaran.NIK', 'pasien.NIK')
     .join('poli', 'pendaftaran.IDPOLI', 'poli.IDPOLI')
     .select(
-        'pendaftaran.IDPENDAFTARAN',
-        'pendaftaran.NIK',
-        'pasien.NAMALENGKAP',
-        'pendaftaran.TANGGALKUNJUNGAN',
-        'pendaftaran.KELUHAN',
-        'pendaftaran.STATUSKUNJUNGAN',
-        'poli.NAMAPOLI as POLI');
-}
+      'pendaftaran.IDPENDAFTARAN',
+      'pendaftaran.NIK',
+      'pasien.NAMALENGKAP',
+      'pendaftaran.TANGGALKUNJUNGAN',
+      'pendaftaran.KELUHAN',
+      'pendaftaran.STATUSKUNJUNGAN',
+      'poli.NAMAPOLI as POLI'
+    );
+};
 
-export const create = (data, trx = db) => {
-  return trx('pendaftaran').insert(data).returning('IDPENDAFTARAN');
-}
+export const create = async (data, trx = db) => {
+  const result = await trx('pendaftaran').insert(data);
+  return result[0];
+};
 
 export const update = (id, data) => {
   return db('pendaftaran').where('IDPENDAFTARAN', id).update(data);
-}
+};
 
 export const remove = (id) => {
   return db('pendaftaran').where('IDPENDAFTARAN', id).del();
-}
+};
 
 export const startTransaction = () => {
   return db.transaction();
