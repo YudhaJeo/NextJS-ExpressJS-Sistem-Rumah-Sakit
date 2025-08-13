6bbc3e8f4860f666154f733bcb35f87feb2d8975
BagasYudha <bayud.kematian@gmail.com>
2025-08-13 15:45:38 +0700

    UPD: Ranap - Update field table

--- sistem_rs_be/src/migrations/20250717100000_create_obat_inap.js
+++ sistem_rs_be/src/migrations/20250717100000_create_obat_inap.js
@@ -1,29 +1,41 @@
 // src/migrations/20250715130000_create_obat_inap.js
 export const up = function (knex) {
   return knex.schema.createTable('obat_inap', (table) => {
-    table.increments('IDOBATINAP').primary();
-    table
-      .integer('IDRAWATINAP')
-      .unsigned()
-      .notNullable()
-      .references('IDRAWATINAP')
-      .inTable('rawat_inap')
-      .onDelete('CASCADE');
+      table.increments('IDOBATINAP').primary();
+
       table
-      .integer('IDOBAT')
-      .unsigned() 
-      .notNullable()
-      .references('IDOBAT')
-      .inTable('obat')
-      .onDelete('RESTRICT');    
-    table.integer('JUMLAH').notNullable();
-    table.double('HARGA').notNullable();
-    table.double('TOTAL').notNullable();
-    table.timestamps(true, true);
+          .integer('IDRAWATINAP')
+          .unsigned()
+          .notNullable()
+          .references('IDRAWATINAP')
+          .inTable('rawat_inap')
+          .onDelete('CASCADE');
+
+      table
+          .integer('IDOBAT')
+          .unsigned()
+          .notNullable()
+          .references('IDOBAT')
+          .inTable('obat')
+          .onDelete('RESTRICT');
+
+      table
+          .integer('IDTENAGAMEDIS')
+          .unsigned()
+          .notNullable()
+          .references('IDTENAGAMEDIS')
+          .inTable('master_tenaga_medis')
+          .onDelete('RESTRICT');
+
+      table.dateTime('WAKTUPEMBERIAN').notNullable();
+
+      table.integer('JUMLAH').notNullable();
+      table.double('HARGA').notNullable();
+      table.double('TOTAL').notNullable();
+      table.timestamps(true, true);
   });
 };
 
 export const down = function (knex) {
   return knex.schema.dropTableIfExists('obat_inap');
 };
-
--- sistem_rs_be/src/migrations/20250718130000_create_tindakan_inap.js.js
+++ sistem_rs_be/src/migrations/20250718130000_create_tindakan_inap.js.js
@@ -1,30 +1,41 @@
 // src/migrations/20250718130000_create_tindakan_inap.js
 export const up = function (knex) {
-    return knex.schema.createTable('tindakan_inap', (table) => {
+  return knex.schema.createTable('tindakan_inap', (table) => {
       table.increments('IDTINDAKANINAP').primary();
+
       table
-        .integer('IDRAWATINAP')
-        .unsigned()
-        .notNullable()
-        .references('IDRAWATINAP')
-        .inTable('rawat_inap')
-        .onDelete('CASCADE');
-        table
-        .integer('IDTINDAKAN')
-        .unsigned() 
-        .notNullable()
-        .references('IDTINDAKAN')
-        .inTable('tindakan_medis')
-        .onDelete('RESTRICT');    
+          .integer('IDRAWATINAP')
+          .unsigned()
+          .notNullable()
+          .references('IDRAWATINAP')
+          .inTable('rawat_inap')
+          .onDelete('CASCADE');
+
+      table
+          .integer('IDTINDAKAN')
+          .unsigned()
+          .notNullable()
+          .references('IDTINDAKAN')
+          .inTable('tindakan_medis')
+          .onDelete('RESTRICT');
+
+      table
+          .integer('IDTENAGAMEDIS')
+          .unsigned()
+          .notNullable()
+          .references('IDTENAGAMEDIS')
+          .inTable('master_tenaga_medis')
+          .onDelete('RESTRICT');
+
+      table.dateTime('WAKTUPEMBERIAN').notNullable();
+
       table.integer('JUMLAH').notNullable();
       table.double('HARGA').notNullable();
       table.double('TOTAL').notNullable();
       table.timestamps(true, true);
-    });
-  };
-  
-  export const down = function (knex) {
-    return knex.schema.dropTableIfExists('tindakan_inap');
-  };
-  
-  
+  });
+};
+
+export const down = function (knex) {
+  return knex.schema.dropTableIfExists('tindakan_inap');
+};
