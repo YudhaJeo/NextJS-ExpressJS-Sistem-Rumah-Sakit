7d72ac8f463e0028beef013f48d2190ba97b6174
BagasYudha <bayud.kematian@gmail.com>
2025-08-26 11:15:22 +0700

    ADD: Ranap - Menambahkan status filter dan bantuan search

--- sistem_rs_fe/app/(main)/(rawat_inap)/rawat_inap/menu/rawat_inap/page.js
+++ sistem_rs_fe/app/(main)/(rawat_inap)/rawat_inap/menu/rawat_inap/page.js
@@ -3,7 +3,7 @@
 
 import { useEffect, useRef, useState } from 'react';
 import axios from 'axios';
-import HeaderBar from '@/app/components/headerbar';
+import HeaderBar from './components/customHeaderBar';
 import TabelRawatInap from './components/tabelRawatInap';
 import FormRawatInap from './components/formRawatInap';
 import ToastNotifier from '@/app/components/toastNotifier';
@@ -28,6 +28,7 @@ const Page = () => {
   const [endDate, setEndDate] = useState(null);
   const [formRawatInapMode, setFormRawatInapMode] = useState("edit");
   const [selectedRawat, setSelectedRawat] = useState(null)
+  const [statusFilter, setStatusFilter] = useState("");
 
   const defaultForm = {
     IDRAWATINAP: '',
@@ -352,16 +353,37 @@ const Page = () => {
     setData(originalData);
   };
 
-  const handleSearch = (keyword) => {
-    if (!keyword) return fetchRawatInap();
-    const filtered = data.filter((item) =>
-      item.NAMALENGKAP?.toLowerCase().includes(keyword.toLowerCase()) ||
-      item.NOMORBED?.toLowerCase().includes(keyword.toLowerCase()) ||
-      item.NOREKAMMEDIS?.toLowerCase().includes(keyword.toLowerCase()) ||
-      item.STATUS?.toLowerCase().includes(keyword.toLowerCase()) 
-    );
+
+  const statusOptions = [
+    { label: "Semua", value: "SEMUA" },
+    { label: "Aktif", value: "AKTIF" },
+    { label: "Selesai", value: "SELESAI" },
+  ];
+
+  const handleSearch = (keyword, status = statusFilter) => {
+    let filtered = originalData;
+  
+    if (keyword) {
+      filtered = filtered.filter((item) =>
+        item.NAMALENGKAP?.toLowerCase().includes(keyword.toLowerCase()) ||
+        item.NOMORBED?.toLowerCase().includes(keyword.toLowerCase()) ||
+        item.NOREKAMMEDIS?.toLowerCase().includes(keyword.toLowerCase()) ||
+        item.STATUS?.toLowerCase().includes(keyword.toLowerCase())
+      );
+    }
+  
+    if (status !== "SEMUA") {
+      filtered = filtered.filter((item) => item.STATUS === status);
+    }
+  
     setData(filtered);
-  }
+  };
+  
+  const handleStatusChange = (e) => {
+    setStatusFilter(e.value);
+    handleSearch("", e.value); 
+  };
+  
 
   return (
     <div className="card">
@@ -382,6 +404,9 @@ const Page = () => {
           title=""
           placeholder="Cari pasien"
           onSearch={handleSearch}
+          statusFilter={statusFilter}
+          statusOptions={statusOptions}
+          handleStatusChange={handleStatusChange}
         />
       </div>
 
