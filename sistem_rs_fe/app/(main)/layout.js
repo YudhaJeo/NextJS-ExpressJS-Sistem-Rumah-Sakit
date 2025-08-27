86d88368b2f6d958e31d51338de7f15f9e688a43
BagasYudha <bayud.kematian@gmail.com>
2025-08-27 11:45:59 +0700

    UPD: ROOT - Perbaikan bug tampilan prime react

--- sistem_rs_fe/app/(main)/layout.js
+++ sistem_rs_fe/app/(main)/layout.js
@@ -1,13 +1,7 @@
+// D:\MARSTECH\NextJS-ExpressJS-Final-System\sistem_rs_fe\app\(main)\layout.js
+
 import Layout from '../../layout/layout';
 import Providers from '../providers';
-import 'primereact/resources/primereact.css';
-import 'primeflex/primeflex.css';
-import 'primeicons/primeicons.css';
-import '../../styles/layout/layout.scss';
-import '../../styles/demo/Demos.scss';
-import '/public/themes/lara-light-indigo/theme.css';
-import '../globals.css';
-
 
 export const metadata = {
   title: 'Sistem Rumah Sakit',
@@ -19,9 +13,7 @@ export const metadata = {
 export default function AppLayout({ children }) {
   return (
     <Providers>
-      <Layout>
-        {children}
-      </Layout>
+      <Layout>{children}</Layout>
     </Providers>
   );
 }
