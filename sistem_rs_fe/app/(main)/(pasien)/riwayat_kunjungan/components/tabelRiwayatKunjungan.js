80abf059262ec3214cbd220546ce9c07b6fff5e0
AlyaZahra <alyazahs13@gmail.com>
2025-09-03 10:17:09 +0700

    UPD: riwayat kunjungan - fungsi cetak

--- sistem_rs_fe/app/(main)/(pasien)/riwayat_kunjungan/components/tabelRiwayatKunjungan.js
+++ sistem_rs_fe/app/(main)/(pasien)/riwayat_kunjungan/components/tabelRiwayatKunjungan.js
@@ -6,7 +6,6 @@ import { Column } from "primereact/column";
 import { Button } from "primereact/button";
 import { Dialog } from "primereact/dialog";
 import dynamic from "next/dynamic";
-// import AdjustPrintMarginLaporan from "./adjustPrintMarginLaporan";
 import axios from "axios";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
@@ -20,7 +19,6 @@ const TabelRiwayatKunjungan = ({ data, loading }) => {
 
   const PDFViewer = dynamic(() => import("./PDFViewer"), { ssr: false });
 
-  // Format tanggal sederhana
   const formatTanggal = (tanggal) => {
     if (!tanggal) return "-";
     const tgl = new Date(tanggal);
@@ -31,11 +29,12 @@ const TabelRiwayatKunjungan = ({ data, loading }) => {
     });
   };
 
-  // Ambil detail kunjungan untuk cetak
   const handleOpenAdjust = async (rowData) => {
     try {
-      const res = await axios.get(`${API_URL}/riwayat_kunjungan/${rowData.NIK}`);
-      setSelectedRow(res.data.data); // isi semua riwayat pasien
+      const res = await axios.get(
+        `${API_URL}/riwayat_kunjungan/${rowData.NIK}`
+      );
+      setSelectedRow(res.data.data);
       setAdjustDialog(true);
     } catch (err) {
       console.error("Gagal ambil detail:", err);
@@ -56,12 +55,6 @@ const TabelRiwayatKunjungan = ({ data, loading }) => {
           tooltip="Lihat Detail"
         />
       </a>
-      <Button
-        icon="pi pi-sliders-h"
-        className="p-button-sm p-button-warning"
-        onClick={() => handleOpenAdjust(rowData)}
-        tooltip="Atur Margin & Cetak"
-      />
     </div>
   );
 
@@ -81,7 +74,7 @@ const TabelRiwayatKunjungan = ({ data, loading }) => {
         <Column field="NIK" header="NIK" />
         <Column field="NAMALENGKAP" header="Nama Pasien" />
         <Column
-          field="TANGGAL" 
+          field="TANGGAL"
           header="Terakhir Kunjungan"
           body={(r) => formatTanggal(r.TANGGAL)}
         />
@@ -92,19 +85,19 @@ const TabelRiwayatKunjungan = ({ data, loading }) => {
         />
       </DataTable>
 
-            <Dialog
-              visible={jsPdfPreviewOpen}
-              onHide={() => setJsPdfPreviewOpen(false)}
-              modal
-              style={{ width: '90vw', height: '90vh' }}
-              header="Preview PDF"
-            >
-              <PDFViewer
-                pdfUrl={pdfUrl}
-                fileName={fileName}
-                paperSize={selectedRow?.paperSize || 'A4'}
-              />
-            </Dialog>
+      <Dialog
+        visible={jsPdfPreviewOpen}
+        onHide={() => setJsPdfPreviewOpen(false)}
+        modal
+        style={{ width: "90vw", height: "90vh" }}
+        header="Preview PDF"
+      >
+        <PDFViewer
+          pdfUrl={pdfUrl}
+          fileName={fileName}
+          paperSize={selectedRow?.paperSize || "A4"}
+        />
+      </Dialog>
     </>
   );
 };
