e34e73502d22e1382bf287cfd9a5756429ea36b2
BagasYudha <bayud.kematian@gmail.com>
2025-09-08 11:56:13 +0700

    ADD: Riwayat Inap - Fitur print riwayat inap

--- sistem_rs_fe/app/(main)/(rawat_inap)/rawat_inap/menu/riwayat_inap/page.js
+++ sistem_rs_fe/app/(main)/(rawat_inap)/rawat_inap/menu/riwayat_inap/page.js
@@ -2,24 +2,32 @@
 
 import { useEffect, useRef, useState } from 'react';
 import axios from 'axios';
-import Cookies from 'js-cookie';
 import { useRouter } from 'next/navigation';
 import HeaderBar from '@/app/components/headerbar';
 import ToastNotifier from '@/app/components/toastNotifier';
 import TabelRiwayatInap from './components/tabelRiwayatInap';
 import FilterTanggal from '@/app/components/filterTanggal';
+import { Button } from "primereact/button";
+import AdjustPrintMarginLaporan from "./print/adjustPrintMarginLaporan";
+import { Dialog } from "primereact/dialog";
+import dynamic from "next/dynamic";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 const Page = () => {
   const toastRef = useRef(null);
-  const router = useRouter();
 
   const [data, setData] = useState([]);
   const [originalData, setOriginalData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(null);
+
+  const [adjustDialog, setAdjustDialog] = useState(false);
+  const [pdfUrl, setPdfUrl] = useState("");
+  const [fileName, setFileName] = useState("");
+  const [jsPdfPreviewOpen, setJsPdfPreviewOpen] = useState(false);
+  const PDFViewer = dynamic(() => import("./print/PDFViewer"), { ssr: false });
 
   useEffect(() => {
     fetchData();
@@ -86,14 +94,41 @@ const Page = () => {
           resetFilter={resetFilter}
         />
 
-        <HeaderBar
-          title=""
-          placeholder="Cari nama atau NIK..."
-          onSearch={handleSearch}
-        />
+        <div className='flex items-center justify-end'>
+          <Button
+            icon="pi pi-print"
+            className="p-button-warning mt-3"
+            tooltip="Atur Print Margin"
+            onClick={() => setAdjustDialog(true)}
+          />
+          <HeaderBar
+            title=""
+            placeholder="Cari nama atau NIK..."
+            onSearch={handleSearch}
+          />
+        </div>
       </div>
 
       <TabelRiwayatInap data={data} loading={loading} />
+
+      <AdjustPrintMarginLaporan
+        adjustDialog={adjustDialog}
+        setAdjustDialog={setAdjustDialog}
+        selectedRow={null}
+        data={data}
+        setPdfUrl={setPdfUrl}
+        setFileName={setFileName}
+        setJsPdfPreviewOpen={setJsPdfPreviewOpen}
+      />
+      <Dialog
+        visible={jsPdfPreviewOpen}
+        onHide={() => setJsPdfPreviewOpen(false)}
+        modal
+        style={{ width: "90vw", height: "90vh" }}
+        header="Preview PDF"
+      >
+        <PDFViewer pdfUrl={pdfUrl} fileName={fileName} paperSize="A4" />
+      </Dialog>
     </div>
   );
 };
